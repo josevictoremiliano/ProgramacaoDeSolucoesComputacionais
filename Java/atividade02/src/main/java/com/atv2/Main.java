8db9152cfb006193193f2561bb8e85e1fde5e496
Jose Victor <59773190+josevictoremiliano@users.noreply.github.com>
2024-03-19 00:19:56 +0000

    Add menu options and switch statement to Main.java

--- Java/atividade02/src/main/java/com/atv2/Main.java
+++ Java/atividade02/src/main/java/com/atv2/Main.java
@@ -3,5 +3,30 @@ package com.atv2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("----------- Bem Vindo------------");
+        System.out.println("Escolha uma das questões abaixo: ");
+        System.out.println("1 - Questão 1");
+        System.out.println("2 - Questão 2");
+        System.out.println("3 - Questão 3");
+        System.out.println("4 - Questão 4");
+
+        int opcao = 1;
+
+        switch (opcao) {
+            case 1:
+                Questao1.main(args);
+                break;
+            case 2:
+                Questao2.main(args);
+                break;
+            case 3:
+                Questao3.main(args);
+                break;
+            case 4:
+                Questao4.main(args);
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
     }
 }
